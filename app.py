f92530bd3732bb2f61a751e13837782da7e3db2c
YLC <ylc950425@gmail.com>
2026-06-17 23:43:51 +0800

    更新 Cog 檔載入指令的輸入方式
    取消本來的在 Discord 內部輸入前綴指令來載入
    改為在終端機輸入指令

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-import discord
-from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import sys
 import Define.Functions as func
 from Define.Classes import MyBot
 import asyncio
@@ -14,9 +13,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 settings = func.read_json("settings")
 
 
-# bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 bot = MyBot()
-
 
 @bot.event
 async def on_ready():
@@ -27,54 +24,52 @@ async def on_ready():
         print(e)
 
 
-@bot.command()
-async def load(ctx: commands.Context, filename):
-    try:
-        await bot.load_extension(f'Cogs.{filename}')
-        await ctx.send("Done")
-        func.time_print(f"Load {filename}")
+# 終端機指令讀取函式
+async def stdin_reader():
+    # 獲取 event loop
+    loop = asyncio.get_event_loop()
 
-        await bot.tree.sync()
+    while True:
+        # 讀取終端機輸入
+        line = (await loop.run_in_executor(None, sys.stdin.readline)).strip()
 
-    except Exception as e:
-        await ctx.send("Error")
-        func.time_print(f"Load {filename} failed")
-        print(e)
+        # 忽略空值
+        if not line:
+            continue
         
+        # 將指令和參數分離
+        args = line.split()
+        cmd = args.pop(0)
 
-@bot.command()
-async def unload(ctx: commands.Context, filename):
-    try:
-        await bot.unload_extension(f"Cogs.{filename}")
-        await ctx.send("Done")
-        func.time_print(f"Unload {filename}")
+        # 如果沒有參數就跳過
+        if not args:
+            continue
 
-        await bot.tree.sync()
+        # load/reload/unload 指令
+        try:
+            if cmd == "l":
+                await bot.load_extension(f"Cogs.{args[0]}")
+                func.time_print(f"Load {args[0]}.py")
+                await bot.tree.sync()
 
-    except Exception as e:
-        await ctx.send("Error")
-        func.time_print(f"Unload {filename} failed")
-        print(e)
+            elif cmd == "rl":
+                await bot.reload_extension(f"Cogs.{args[0]}")
+                func.time_print(f"Reload {args[0]}.py")
+                await bot.tree.sync()
 
-
-@bot.command()
-async def reload(ctx: commands.Context, filename):
-    try:
-        await bot.reload_extension(f"Cogs.{filename}")
-        await ctx.send("Done")
-        func.time_print(f"Reload {filename}")
-
-        await bot.tree.sync()
-
-    except Exception as e:
-        await ctx.send("Error")
-        func.time_print(f"Reload {filename} failed")
-        print(e)
+            elif cmd == "ul":
+                await bot.unload_extension(f"Cogs.{args[0]}")
+                func.time_print(f"Unload {args[0]}.py")
+                await bot.tree.sync()
+            
+        except Exception as e:
+            func.time_print(f"Error\n{e}")
 
 
 async def main():
     try:
         async with bot:
+            asyncio.create_task(stdin_reader())
             await bot.start(TOKEN)
     except asyncio.CancelledError:
         print("bot中斷")
